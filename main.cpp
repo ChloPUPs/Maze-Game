e424287660d076bf5ddd68e669216d29f38f78d4
ChloPUPs <alexplaysgamezzz@outlook.com>
2025-08-13 17:52:08 -0700

    added renderer

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
 
   SDL_Window* window = SDL_CreateWindow("Maze Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
   bool running = true;
   SDL_Event e;
@@ -25,6 +26,7 @@ int main(int argc, char* argv[]) {
   }
 
   SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
   SDL_Quit();
 
   return 0;
